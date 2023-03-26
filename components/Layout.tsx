a59cce6181c41aec247b0b215d75eb9e25344c71
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-26 20:02:02 +0100

    Add followbar component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import FollowBar from './layout/FollowBar';
 import Sidebar from './layout/Sidebar';
 
 interface LayoutProps {
@@ -25,6 +26,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
           >
             {children}
           </div>
+          <FollowBar />
         </div>
       </div>
     </div>

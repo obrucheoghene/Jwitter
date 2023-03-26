f23847df19f254a415e74e39d947785c1aa8a5a2
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-26 22:01:08 +0100

    Add Login and Register Modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from 'next/app';
 import Layout from '@/components/Layout';
 import '@/styles/globals.css';
+import LoginModal from '@/components/modals/LoginModal';
+import RegisterModal from '@/components/modals/RegisterModal';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <LoginModal />
+      <RegisterModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>

cfa118bb431b7309a65e6be9fc3713566128eff3
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-27 08:33:35 +0100

    Add toaster and next js session provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from 'next/app';
+import { Toaster } from 'react-hot-toast';
+import { SessionProvider } from 'next-auth/react';
 import Layout from '@/components/Layout';
 import '@/styles/globals.css';
 import LoginModal from '@/components/modals/LoginModal';
@@ -6,12 +8,13 @@ import RegisterModal from '@/components/modals/RegisterModal';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
+      <Toaster />
       <LoginModal />
       <RegisterModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </SessionProvider>
   );
 }

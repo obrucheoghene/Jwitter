8951c7ff3541e46268e452d50fa4064120934b33
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-01 01:50:34 +0100

    Create image upload component

---
+++ components/ImageUpload.tsx
@@ -0,0 +1,40 @@
+import { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+
+interface ImageUploadProps {
+  onChange: (base64: string) => void;
+  label: string;
+  value?: string;
+  disabled?: boolean;
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({
+  onChange,
+  label,
+  value,
+  disabled,
+}) => {
+  const [base64, setBase64] = useState(value);
+  const handleChange = useCallback(
+    (base64: string) => {
+      onChange(base64);
+    },
+    [onChange]
+  );
+
+  const handleDrop = useCallback(
+    (files: any) => {
+      const file = files[0];
+      const reader = new FileReader();
+      reader.onload = (event: any) => {
+        setBase64(event.target.result);
+        handleChange(event.target.result);
+      };
+      reader.readAsDataURL(file);
+    },
+    [handleChange]
+  );
+  return <div>ImageUpload</div>;
+};
+
+export default ImageUpload;

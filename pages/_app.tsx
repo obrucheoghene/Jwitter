45309c81fef95710082c7abb097531ae4f98c45a
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-01 01:51:15 +0100

    Add edit modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import Layout from '@/components/Layout';
 import '@/styles/globals.css';
 import LoginModal from '@/components/modals/LoginModal';
 import RegisterModal from '@/components/modals/RegisterModal';
+import EditModal from '@/components/modals/EditModal';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Toaster />
       <LoginModal />
       <RegisterModal />
+      <EditModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>

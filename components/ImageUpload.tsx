175a55fbed025dad1ca03d8ed342f7ceadb07b17
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-02 23:37:54 +0100

    Update ImageUpload component

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { useCallback, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 
@@ -44,7 +45,23 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
       'image/png': [],
     },
   });
-  return <div>ImageUpload</div>;
+  return (
+    <div
+      {...getRootProps({
+        className:
+          'w-full p-4 text-white text-center border-2 border-dotted rounded-md border-neutral-700',
+      })}
+    >
+      <input {...getInputProps()} />
+      {base64 ? (
+        <div className=" flex item-center justify-center">
+          <Image src={base64} height="100" width="100" alt="Uploaded image" />
+        </div>
+      ) : (
+        <p className=" text-white">{label}</p>
+      )}
+    </div>
+  );
 };
 
 export default ImageUpload;

e2a8b7c224817060df1e07c6e26e9a0bb5193dbf
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-03 00:03:32 +0100

    Handle get posts route

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -25,6 +25,37 @@ export default async function handler(
       });
       return res.status(200).json(post);
     }
+
+    if (req.method === 'GET') {
+      const { userId } = req.query;
+
+      let posts;
+      if (userId && typeof userId === 'string') {
+        posts = await prisma.post.findMany({
+          where: {
+            userId,
+          },
+          include: {
+            user: true,
+            comments: true,
+          },
+          orderBy: {
+            createdAt: 'desc',
+          },
+        });
+      } else {
+        posts = await prisma.post.findMany({
+          include: {
+            user: true,
+            comments: true,
+          },
+          orderBy: {
+            createdAt: 'desc',
+          },
+        });
+      }
+      return res.status(200).json(posts);
+    }
   } catch (error) {
     console.log(error);
     return res.status(400).end();

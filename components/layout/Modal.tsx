542ed24074b701fef43b732842bfa22f69aa9d9c
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-02 23:37:35 +0100

    Debug modal opacity issue

--- components/layout/Modal.tsx
+++ components/layout/Modal.tsx
@@ -40,7 +40,7 @@ const Modal: React.FC<ModalProps> = ({
     <>
       <div
         className="justify-center items-center flex overflow-x-hidden 
-        overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800  bg-opacity-70"
+        overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800 bg-opacity-70"
       >
         <div className="relative w-full lg:w-3/6 my-6 mx-auto lg:max-w-3xl h-full lg:h-auto ">
           {/*  Content */}

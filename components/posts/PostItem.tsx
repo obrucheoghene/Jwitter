e21fd5c08380fc4fa6b2cc5b2dc151c1244530a9
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-07 20:40:27 +0100

    Update PostItem component

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -1,10 +1,13 @@
 import useCurrentUser from '@/hooks/useCurrentUsers';
 import useLoginModal from '@/hooks/useLoginModal';
+import { formatDistanceToNowStrict } from 'date-fns';
 import { useRouter } from 'next/router';
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
+import { AiOutlineHeart, AiOutlineMessage } from 'react-icons/ai';
+import Avatar from '../Avatar';
 
 interface PostItemProps {
-  userId: string;
+  userId?: string;
   data: Record<string, any>;
 }
 const PostItem: React.FC<PostItemProps> = ({ userId, data }) => {
@@ -22,8 +25,68 @@ const PostItem: React.FC<PostItemProps> = ({ userId, data }) => {
 
   const gotoPost = useCallback(() => {
     router.push(`/posts/${data.id}`);
-  }, []);
-  return <div>PostItem</div>;
+  }, [router, data.id]);
+
+  const onlike = useCallback(
+    (event: any) => {
+      event.stopPropagation();
+
+      loginModal.onOpen();
+    },
+    [loginModal]
+  );
+
+  const createdAt = useMemo(() => {
+    if (!data?.createdAt) {
+      return null;
+    }
+    return formatDistanceToNowStrict(new Date(data.createdAt));
+  }, [data?.createdAt]);
+  return (
+    <div
+      onClick={gotoPost}
+      className="border-b-[1px]
+  border-neutral-800 p-5 cursor-pointer hover:bg-neutral-900 transition"
+    >
+      <div className="flex flex-row item-start gap-3">
+        <Avatar userId={data.user.id} />
+        <div>
+          <div className=" flex flex-row items-center gap-2">
+            <p
+              onClick={gotoUser}
+              className="text-white
+                font-semibold cursor-pointer hover:underline"
+            >
+              {data.user.name}
+            </p>
+            <span
+              onClick={gotoUser}
+              className=" text-neutral-500 
+            cursor-pointer hover:underline hidden md:block"
+            >
+              @{data.user.username}
+            </span>
+            <span className=" text-neutral-500 text-sm">{createdAt}</span>
+          </div>
+          <div className=" text-white mt-1">{data.body}</div>
+          <div className=" flex flex-row items-center mt-3 gap-10">
+            <div className=" flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-sky-500">
+              <AiOutlineMessage size={20} />
+              <p>{data.comments?.length || 0}</p>
+            </div>
+
+            <div
+              onClick={onlike}
+              className=" flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-red-500"
+            >
+              <AiOutlineHeart size={20} />
+              <p>{data.comments?.length || 0}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostItem;

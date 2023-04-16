d5185248900ec45df66da3b0c7be1dd66cf8f573
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-16 19:08:02 +0100

    Implement notification on follow

--- pages/api/follow.ts
+++ pages/api/follow.ts
@@ -30,6 +30,26 @@ export default async function name(req: NextApiRequest, res: NextApiResponse) {
 
     if (req.method === 'POST') {
       updatedFollowingIds.push(userId);
+
+      try {
+        await prisma.notification.create({
+          data: {
+            body: 'Someone followed you!',
+            userId,
+          },
+        });
+
+        await prisma.user.update({
+          where: {
+            id: userId,
+          },
+          data: {
+            hasNotification: true,
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      }
     }
 
     if (req.method === 'DELETE') {

c5685c9a4f34140e4af20176e15eeac378771bc9
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-22 16:38:20 +0100

    fix: filter out current user from who to follow

--- components/layout/FollowBar.tsx
+++ components/layout/FollowBar.tsx
@@ -1,23 +1,30 @@
+import useCurrentUser from '@/hooks/useCurrentUsers';
 import useUsers from '@/hooks/useUsers';
 import Avatar from '../Avatar';
 
 const FollowBar = () => {
   const { data: users = [] } = useUsers();
+  const { data: currentUser } = useCurrentUser();
+
   if (users.length === 0) return null;
   return (
     <div className=" px-6 py-4 hidden lg:block">
       <div className=" bg-neutral-800 rounded-xl p-4">
         <h2 className="text-white text-xl font-semibold">Who to follow</h2>
         <div className="flex flex-col gap-6 mt-4">
-          {users.map((user: Record<string, any>) => (
-            <div key={user.id} className="flex flex-row gap-4">
-              <Avatar userId={user.id} />
-              <div className="flex flex-col">
-                <p className="text-white font-semibold text-sm">{user.name}</p>
-                <p className="text-neutral-400 text-sm">@{user.username}</p>
+          {users
+            .filter((user: Record<string, any>) => user.id !== currentUser?.id)
+            .map((user: Record<string, any>) => (
+              <div key={user.id} className="flex flex-row gap-4">
+                <Avatar userId={user.id} />
+                <div className="flex flex-col">
+                  <p className="text-white font-semibold text-sm">
+                    {user.name}
+                  </p>
+                  <p className="text-neutral-400 text-sm">@{user.username}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </div>
